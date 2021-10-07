d808331a350c932260da69492a88f4e5650e83ff
Julia <222596@edu.p.lodz.pl>
2021-10-07 14:19:55 +0200

    Napisanie metody reverseText do odwracania ciągu znakow

--- src/main/java/com/julia/lab1/Controllers/Controller.java
+++ src/main/java/com/julia/lab1/Controllers/Controller.java
@@ -1,10 +1,22 @@
 package com.julia.lab1.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @GetMapping("/reverse")
+    public String reverseText(@RequestParam("text") String text){
 
+        StringBuilder sb = new StringBuilder(text);
+        sb.reverse();
+        String text2 = sb.toString();
+        System.out.println("Original string: " + text);
+        System.out.println("Reversed string: " + text2);
+
+        return text2;
+    }
 
 }
